62cf496baaa2b4c73c86378911d13e9f634ee8ac
Ganesh Bangalore <gbangalore@GBANGALORE-DEV0.localdomain>
2026-09-04 15:19:35 -0700

    fix cuda for gbangalore-dev0

--- cppsetup/add_arrays.cpp
+++ cppsetup/add_arrays.cpp
@@ -1,6 +1,14 @@
 #include <print>
 
 int main() {
-    std::print("Hello World");
+    using namespace std;
+    for (int i=0; i<100; i++) {
+        std::println("1<<{}: {}", i, 1<<i);
+    }
+
+    println("{}",INTMAX_MAX);
+
+    int N = 1<<2;
+    std::print("Hello World {}", N);
     return 0;
 }
